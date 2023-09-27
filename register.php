ab9bd1dd0063434378452af98ec9226df9aa742a
kevin-magu <wanjaukevinmagu@gmail.com>
2023-09-27 12:03:38 +0300

    updates

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="register.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="register-card">
+        <div class="title"></div>
+        <div class="input-fields">
+            <form action="">
+                <label for="name">Name</label>
+                <input id="name" type="text">
+                <label for="regno">Email</label>
+                <input id="email" type="email">
+                <label for="phone">Phone</label>
+                <input id="phone" type="number">
+                <label for="mac">MAC</label>
+                <input id="mac" type="text">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
