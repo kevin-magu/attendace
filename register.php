a60b9a1c11c125f3c31f902e4961b5f2db66c6bb
kevin-magu <wanjaukevinmagu@gmail.com>
2023-09-27 12:38:27 +0300

    u

--- register.php
+++ register.php
@@ -4,13 +4,13 @@
     <link rel="stylesheet" href="register.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
 </head>
 <body>
     <div class="register-card">
-        <div class="title"></div>
+        <div class="title">KUCSA REGISTRATION</div>
         <div class="input-fields">
-            <form action="">
+            <form action="" method="post">
                 <label for="name">Name</label>
                 <input id="name" type="text">
                 <label for="regno">Email</label>
@@ -19,6 +19,7 @@
                 <input id="phone" type="number">
                 <label for="mac">MAC</label>
                 <input id="mac" type="text">
+                <div class="button"><button>Register</button></div>
             </form>
         </div>
     </div>
